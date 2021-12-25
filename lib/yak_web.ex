74baf6a1325415a0524674c4d90560b7e01adb6a
Nauman <recluze@gmail.com>
2021-12-25 08:57:43 +0500

    Added live support through mix

--- lib/yak_web.ex
+++ lib/yak_web.ex
@@ -91,6 +91,7 @@ defmodule YakWeb do
 
       # Import LiveView and .heex helpers (live_render, live_patch, <.form>, etc)
       import Phoenix.LiveView.Helpers
+      import YakWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
---
+++ lib/yak_web/live/post_live/show.ex
@@ -0,0 +1,21 @@
+defmodule YakWeb.PostLive.Show do
+  use YakWeb, :live_view
+
+  alias Yak.Timeline
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_params(%{"id" => id}, _, socket) do
+    {:noreply,
+     socket
+     |> assign(:page_title, page_title(socket.assigns.live_action))
+     |> assign(:post, Timeline.get_post!(id))}
+  end
+
+  defp page_title(:show), do: "Show Post"
+  defp page_title(:edit), do: "Edit Post"
+end
